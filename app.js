4914a882bb6516139c051957c6aadb685f15dc4d
Cristhian Silva <silvasilvacristhian.3@gmail.com>
2025-05-13 12:48:06 -0500

    Initial

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("API Node.js funcionando desde AWS");
+});
+
+app.listen(port, () => {
+  console.log(`Servidor ejecutándose en puerto ${port}`);
+});
