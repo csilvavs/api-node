dbccd2e4b73bdddad185bf800bb4679e180a9ffd
Cristhian Silva <silvasilvacristhian.3@gmail.com>
2025-05-13 15:15:14 -0500

    Initial

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("API Node.js funcionando desde AWS");
+  res.send("API Node.js funcionando desde AWS cap de prueba Vnetura Soluciones");
 });
 
 app.listen(port, () => {
