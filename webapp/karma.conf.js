b9ce88b29347548c0958473ec87d164146d65217
Cristhian Silva <silvasilvacristhian.3@gmail.com>
2025-05-15 14:53:44 -0500

    comi

---
+++ webapp/karma.conf.js
@@ -0,0 +1,20 @@
+module.exports = function(config) {
+  config.set({
+    frameworks: ["ui5"],
+    browsers: ["ChromeHeadless"],
+    singleRun: true,
+
+    ui5: {
+      type: "application",
+      configPath: "ui5.yaml",
+      testpage: "/test/unit/unitTests.qunit.html"
+    },
+
+    reporters: ["progress"],
+    plugins: [
+      "karma-ui5",
+      "karma-qunit",
+      "karma-chrome-launcher"
+    ]
+  });
+};
